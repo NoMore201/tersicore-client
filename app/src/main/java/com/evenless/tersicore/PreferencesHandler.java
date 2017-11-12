41e60c4888dc618967654162226155253bd8c944
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2017-11-12 15:36:54 +0100

    Initial commit

---
+++ app/src/main/java/com/evenless/tersicore/PreferencesHandler.java
@@ -0,0 +1,22 @@
+package com.evenless.tersicore;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class PreferencesHandler {
+    private static final String PREF_SERVER_KEY = "TersicoreServer";
+    private static final String PREF_TOKEN_KEY = "TersicoreToken";
+
+    static SharedPreferences getSharedPreferences(Context ctx) {
+        return PreferenceManager.getDefaultSharedPreferences(ctx);
+    }
+
+    public static String getServer(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_SERVER_KEY, null);
+    }
+
+    public static String getToken(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_TOKEN_KEY, null);
+    }
+}
