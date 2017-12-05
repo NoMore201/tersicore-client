8f5ec3811046c436df9dc9a1bcca98356add222d
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2017-12-05 11:45:10 +0100

    TaskHandler: added function to check server status

--- app/src/main/java/com/evenless/tersicore/TaskHandler.java
+++ app/src/main/java/com/evenless/tersicore/TaskHandler.java
@@ -1,18 +1,25 @@
 package com.evenless.tersicore;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.evenless.tersicore.model.Track;
 import com.evenless.tersicore.tasks.ApiRequestTask;
 import com.evenless.tersicore.tasks.CoverRetrieveTask;
 import com.evenless.tersicore.tasks.ImageRequestTask;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class TaskHandler {
+    private static final String TAG = "TaskHandler";
     private static final String API_BASE_URL = "https://ws.audioscrobbler.com/2.0/";
     private static final String API_KEY = "b6570587abc105bc286cf227cabbba50";
     private static final String API_SHARED_SECRET = "ca91306bbff831733d675dfc6e556b77";
@@ -57,6 +64,40 @@ public class TaskHandler {
         String url = "https://" + server + "/stream/" + track.resources.get(0).uuid;
         CoverRetrieveTask task = new CoverRetrieveTask(listener, track, url, id);
         task.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
+    }
+
+    public static boolean checkServerIsRunning(String serverUrl) {
+        HttpsURLConnection connection = null;
+        String result = null;
+        StringBuilder sb;
+        try {
+            URL url = new URL("https://" + serverUrl);
+            connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+            if (responseCode != HttpsURLConnection.HTTP_OK) {
+                throw new IOException("HTTP error code: " + responseCode);
+            }
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+            result = sb.toString();
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return result != null && result.contains("Tersicore");
     }
 
     private static String buildArtistUrl(String artist) throws UnsupportedEncodingException {
